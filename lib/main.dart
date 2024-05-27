a5f21e92dd99e841a022e80369b4889f6ad12691
Iccafw <2200016125@webmail.uad.ac.id>
2024-05-27 15:55:44 +0700

    second change

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Fetch Data Example',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.blue,
       ),
       home: const HomePage(),
     );
